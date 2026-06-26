5d798216908628012fef5f70b885c5db5fd659ee
Rw9. <lucaskulig11@gmail.com>
2026-06-27 00:51:21 +0200

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,15 @@
+{
+  "name": "neonquest",
+  "version": "1.0.0",
+  "description": "NeonQuest Multiplayer Game",
+  "main": "server/server.js",
+  "scripts": {
+    "start": "node server/server.js",
+    "dev": "nodemon server/server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "ws": "^8.14.2",
+    "uuid": "^9.0.0"
+  }
+}
